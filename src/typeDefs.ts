2a301c2619a38ec9d66ab6f601524be582ea6b43
Andrew Cline <acline@arigs.com>
2020-08-17 15:19:01 -0500

    add mutation types

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,6 +1,11 @@
 import { gql } from 'apollo-server'
 
 export const typeDefs = gql`
+  type Query {
+    books(id: String, title: String): [Book]
+    authors(id: String, name: String): [Author]
+  }
+
   type Book {
     _id: String
     title: String
@@ -13,8 +18,13 @@ export const typeDefs = gql`
     books: [Book]
   }
 
-  type Query {
-    books(id: String, title: String): [Book]
-    authors(id: String, name: String): [Author]
+  type Mutation {
+    createBook(args: { title: String, author: String }): Book
+    updateBook(id: String, args: { title: String, author: String }): Book
+    deleteBook(id: String): String
+
+    createAuthor(String, args: { name: String, books: Book[] }): Author
+    updateAuthor(id: String, args: { name: String, books: Book[] }: Author
+    deleteAuthor(id: String): String
   }
 `
