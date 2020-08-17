4c2019fee6c7ba48ac8cae081a77e159037cadfa
Andrew Cline <acline@arigs.com>
2020-08-17 12:10:04 -0500

    add crud functions for authors

--- __tests__/authors.test.ts
+++ __tests__/authors.test.ts
@@ -1,28 +1,95 @@
-import { makeAuthors } from '../src/lib/authors'
 import { makeDB } from '../src/lib/db'
+import * as shortid from 'shortid'
+
+import { makeAuthors } from '../src/lib/authors'
 
 describe('authors db interface', () => {
-  const db = makeDB({
-    authors: [
-      {
-        name: 'J.K. Rowling',
-        books: [1, 2, 3, 4]
-      }
-    ]
+  let seedId: string
+  let seedAuthors: { 
+    authors: Author[]
+  }
+  let db  
+  let Authors
+
+  beforeEach(() => {
+    seedId = shortid.generate()
+    seedAuthors = {
+      authors: [
+        {
+          _id: seedId,
+          name: 'J.K. Rowling',
+          books: [1, 2, 3, 4]
+        }
+      ]
+    }
+    db = makeDB(seedAuthors)
+    Authors = makeAuthors(db)
   })
 
-  const Authors = makeAuthors(db)
-
-  it('should have getAuthors Function', () => {
-    expect(JSON.stringify(Authors.getAll())).toEqual(
-      JSON.stringify(
+  it('should be able to get all authors', () => {
+    expect(Authors.getAll()).toEqual(
         [
           {
+            _id: seedId,
             name: 'J.K. Rowling',
             books: [1, 2, 3, 4]
           }
         ]
-      )
     )
+  })
+
+  it('should be able to create authors', () => {
+    const lewis = {
+      name: 'C.S. Lewis',
+      books: ['1']
+    }
+
+    const newAuth = Authors.create('C.S. Lewis', ['1'])
+    const authors = Authors.getAll()
+
+    expect(authors).toHaveLength(2)
+
+    expect(authors).toEqual(
+      expect.arrayContaining([expect.objectContaining(lewis)])
+    )
+
+    expect(newAuth).toEqual(
+      expect.objectContaining(lewis)
+    )
+  })
+
+  it('should be able to get Author By id', () => {
+    expect(Authors.getById(seedId)).toEqual(seedAuthors.authors[0])
+  })
+
+  it('should be able to update an author', () => {
+    const updatedAuthor = Authors.update(
+      seedId, 
+      {
+        name: seedAuthors.authors[0].name,
+        books: [...seedAuthors.authors[0].books, 5],
+      }
+    )
+
+    expect(updatedAuthor).toEqual({
+      _id: seedId,
+      name: seedAuthors.authors[0].name,
+      books: [1, 2, 3, 4, 5],
+    })
+
+    const updates = Authors.getById(seedId)
+
+    expect(updates).toEqual({
+      _id: seedId,
+      name: seedAuthors.authors[0].name,
+      books: [1, 2, 3, 4, 5]
+    })
+  })
+
+  it('should be able to delete an author', () => {
+    const id = Authors.delete(seedId)
+
+    expect(id).toEqual(seedId)
+    expect(Authors.getAll()).toHaveLength(0)
   })
 })
--- src/lib/authors.ts
+++ src/lib/authors.ts
@@ -1,10 +1,54 @@
+import * as shortid from 'shortid'
+
+type Author = {
+  _id: string
+  name: string
+  books: string[]
+}
+
 export function makeAuthors (db) {
   return {
     getAll: () => {
-      return db.get('authors')
+      return db.get('authors').value()
     },
-    getById: () => { },
-    update: () => { },
-    delete: () => { }
+    getById: (id: string) => {
+      return db.get('authors').find({ _id: id }).value()
+    },
+    create: (name: string, books: string[]) => {
+      const author = {
+        _id: shortid.generate(),
+        name,
+        books,
+      }
+
+      db.get('authors').push(author).write()
+
+      return author
+    },
+    update: (
+      id: string,
+      updates: { books: string[], name: string }
+    ) => {
+
+      db.get('authors')
+        .find({ _id: id })
+        .assign(updates)
+        .write()
+
+      const author = {
+        _id: id,
+        books: updates.books,
+        name: updates.name
+      }
+
+      return author
+    },
+    delete: (id: string) => {
+      db.get('authors')
+        .remove({ _id: id })
+        .write()
+
+        return id
+    }
   }
 }
