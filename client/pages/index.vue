a7b5dc2ea71382470f3f4f692541372438482cdd
Andrew Cline <acline@arigs.com>
2020-08-18 14:57:11 -0500

    begin work on author client

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <ul>
+        <li v-for="author in authors" :key="author._id" class="author">
+          {{ author.name }}
+        </li>
+      </ul>
+      <button class="add-btn" @click="toggleAddAuthor">+</button>
+      <div v-if="addAuthorForm">
+        <input
+          class="border border-solid rounded px-5 py-2"
+          v-model="authorName"
+        />
+        <button class="submit-btn" @click="submitAddAuthor">Submit</button>
       </div>
     </div>
   </div>
@@ -29,8 +20,35 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import gql from 'graphql-tag'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      addAuthorForm: false,
+      authorName: '',
+    }
+  },
+  methods: {
+    toggleAddAuthor() {
+      this.addAuthorForm = !this.addAuthorForm
+    },
+    submitAddAuthor() {
+      console.log(`added ${this.authorName}`)
+      this.addAuthorForm = false
+    },
+  },
+  apollo: {
+    authors: gql`
+      query {
+        authors {
+          _id
+          name
+        }
+      }
+    `,
+  },
+})
 </script>
 
 <style>
@@ -38,17 +56,21 @@ export default Vue.extend({})
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 
+.author {
+  @apply text-2xl font-bold;
+}
+
+.add-btn {
+  @apply text-2xl font-bold bg-green-400 px-5 py-2 mt-2 rounded-full;
+}
+
+.submit-btn {
+  @apply bg-green-400 px-5 py-2 mt-2 rounded;
+}
+
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
